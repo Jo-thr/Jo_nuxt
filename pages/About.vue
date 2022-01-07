aa5388275c9be061f26a0ec563b88337169061fd
Jo-thr <jonathan.thuayre@gmail.com>
2022-01-07 11:11:40 +0100

    add content into about page

--- pages/About.vue
+++ pages/About.vue
@@ -7,15 +7,16 @@
       w-full
       bg-offwhite
       font-paragraph
+      text-center
     "
   >
+    <!-- HEADER SECTION -->
     <div class="container flex flex-col items-center">
       <div class="bg-blue w-full bg-header-about mt-40"></div>
       <div
         class="
           bg-offwhite
           w-70%
-          text-center
           pt-12
           -mt-14
           font-fontTitle
@@ -25,55 +26,90 @@
           text-blue
         "
       >
-        Tout savoir sur moi
+        {{ about.header.title }}
       </div>
-      <p class="w-70% text-justify my-14 text-desktop-paragraph">
-        Depuis plusieurs années, je suis connecté professionnellement à
-        l'industrie du développement Web et au design d'interfaces.<br /><br />
-
-        Personne bien organisée, plutôt cartisienne et structurée, j'aime
-        trouver la solution aux problèmes. Employé chez One4You depuis plus de 2
-        ans et demi avec en parallèle une expérience d'indépendant. Intéressé
-        par l’ensemble du spectre front-end et travaillant sur des projets
-        d’ambitions. J’aime être au plus proche d’un rendu réaliste et
-        réalisable pour un développeur, prendre en compte toutes les
-        contraintes, tout en portant une grande attention aux détails.
-        <br /><br />
-        Durant mes temps libres, je porte la casquette de joueur de basket, de
-        photographe amateur. Je pratique de nombreuses activités en plein air,
-        adore la curiosité, la littérature et, plus récemment, jeune promeneur
-        de chien.
+      <div class="mt-10 w-52 border-t-2 border-solid border-blue"></div>
+      <p class="w-70% mt-10 text-desktop-paragraph">
+        {{ about.header.description }}
       </p>
+      <div class="flex flex-row my-16">
+        <div
+          v-for="(spe, index) in about.header.specialities"
+          :key="index"
+          class="flex flex-col justify-center items-center px-4"
+        >
+          <div class="h-2 w-40 bg-blue"></div>
+          <p class="mt-2 text-blue italic">{{ spe.name }}</p>
+        </div>
+      </div>
     </div>
 
+    <!-- BRING SECTION -->
     <div class="bg-blue w-full h-60v mt-2"></div>
-
     <div class="container flex flex-col items-center">
-      <p class="w-70% text-justify my-14 text-desktop-paragraph">
-        D'abord Designer d'interface, je me suis peu à peu spécialisé dans le
-        développement front-end pour lequel j'ai développé une grande passion au
-        point de vouloir m'y consacrer aujourd'hui : à temps plein !
-        <br /><br />
-        Polyvalent et adaptable à n’importe quel type de situation. Je n’ai
-        jamais échoué un projet. Je pense que cela vient du fait d’une bonne
-        communication au sein de l’équipe avec laquelle je travaille et je
-        n’hésite pas à challenger les décisions prisent si j’estime que le
-        projet pourrait bénéficier d’idées plus pertinentes tout en s'appuyant
-        sur des données.
-        <br /><br />
-        L'expertise et la communication sont les clés d'un projet réussi. Deux
-        domaines dans lesquels vous pouvez me faire confiance.
+      <p class="w-70% mt-14 text-desktop-paragraph">
+        {{ about.bring.intro }}
       </p>
+      <div class="mt-10 mb-14">
+        <div
+          v-for="(value, index) in about.bring.values"
+          :key="index"
+          class="flex flex-row items-center justify-center"
+        >
+          <!-- LEFT CONTAINER -->
+          <div
+            class="
+              flex flex-col
+              text-right
+              border-r-2 border-solid border-blue
+              justify-end
+              items-end
+              my-10
+              p-5
+              w-40
+            "
+          >
+            <div
+              class="
+                flex
+                bg-blue
+                mb-2
+                w-10
+                h-10
+                text-white
+                items-center
+                justify-center
+              "
+            >
+              {{ value.id }}
+            </div>
+            {{ value.title }}
+          </div>
+
+          <!-- RIGHT CONTAINER -->
+          <div class="p-5 text-left w-30%">
+            {{ value.description }}
+          </div>
+        </div>
+      </div>
     </div>
 
+    <!-- SKILLS SECTION -->
     <div class="bg-blue w-full h-80 mt-2"></div>
+    <div class="mt-14 mb-40 w-40%">{{ about.skills.intro }}</div>
   </div>
 </template>
 
 <script>
+import datas from '@/datas/data.json'
 export default {
   name: 'About',
   layout: 'page',
+  data() {
+    return {
+      about: datas.about,
+    }
+  },
   head() {
     return {
       title: 'À propos | Jonathan Thr',
