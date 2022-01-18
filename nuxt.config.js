0ad0073c12d8e11f5f79e193518df878f66c496a
Jo-thr <jonathan.thuayre@gmail.com>
2022-01-18 12:42:32 +0100

    add transition page

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,14 +23,17 @@ export default {
       },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
+      { rel: 'apple-touch-icon', href: '/favicon.png' },
+    ],
   },
 
   generate: {
     fallback: true,
   },
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['@assets/css/index.css'],
+  css: ['@assets/css/index.css', '@assets/css/transition.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
